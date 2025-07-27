744775ad6aa95335991a15f4776634a87bedacc1
Kelvin Foster <20197992@s.ubaguio.edu>
2025-07-28 02:21:04 +0800

    v1.8

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,9 +19,21 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://sasm-hresd3wls-fosterkelvins-projects.vercel.app",
+];
+
 app.use(
   cors({
-    origin: APP_ORIGIN,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
